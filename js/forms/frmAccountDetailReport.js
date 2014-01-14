0912bb3537eb12c8a5aa2ef833b936d0648ef757
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2014-01-14 09:39:40 +0100

    Radio Button: disable div with controls that depend on the button

---
+++ js/forms/frmAccountDetailReport.js
@@ -0,0 +1,9 @@
+$("input[name=periodrange]").change(function() {
+    $('#divPeriod :input').attr('disabled', $(this).attr('value') != "period");
+    $('#divQuarter :input').attr('disabled', $(this).attr('value') != "quarter");
+    $('#divDate :input').attr('disabled', $(this).attr('value') != "date");
+});
+
+$(document).ready(function() {
+    $("input[value=period]").click();
+});
